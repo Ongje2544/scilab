3db6408080b622b32c4f50d9cf77c36dbccea050
Ongje2544 <6321207009@bsru.ac.th>
2024-05-08 20:18:44 +0700

    first upload

---
+++ application/controllers/dashboard.php
@@ -0,0 +1,23 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+class dashboard extends CI_Controller
+{
+
+	public function __construct()
+	{
+		parent::__construct();
+		// $this->load->model('check_login_model');
+		// $this->load->model('fines_model');
+		// $this->load->model('province_model');
+		// $this->load->model('organization_model');
+	}
+
+	public function index()
+	{
+		$view["module"] = $this->load->view("backend/dashboard/list", NULL, TRUE);
+		$this->load->view("backend/template", $view);
+	}
+
+}
+// echo '1';
+// exit();
